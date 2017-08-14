b66e8e22470a995053e3619f979406ca32c0883b
Helge Fylkesnes <helge_fylkesnes@hotmail.com>
2017-08-14 16:40:10 +0200

    Lærte jQuerry

--- skarnsundet/index.js
+++ skarnsundet/index.js
@@ -1,7 +1,24 @@
+// IIFE - Immediately Invoked Function Expression
+(function(yourcode) {
+
+    // The global jQuery object is passed as a parameter
+    yourcode(window.jQuery, window, document);
+
+  }(function($, window, document) {
+
+    // The $ is now locally scoped 
+
+   // Listen for the jQuery ready event on the document
+   $(function() {
+     
+    $('h1').click(function(){
+      console.log('klikk');
+    });
+    //DOM code
 
 
-$(document).ready(function(){
-  $('h1').click(function(){
-    console.log('klikk');
-  });
-});
+   });
+
+   // annen kode
+  
+}));
